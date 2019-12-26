93c0455208a8779277343aed5c361035841e2081
Sofia Kalinichenko <kalinichenko@pixelfield.cz>
2019-12-26 21:57:05 +0100

    second – useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import TodoList from './TodoList'
 
 export default function App()  {
 
-  const [todos, setTodos] = useState([
-    {id: 1, title: 'First todo', completed: false},
-    {id: 2, title: 'Second todo', completed: true},
-  ])
-
+  const [todos, setTodos] = useState([])
   const [todoTitle, setTodoTitle] = useState('')
+
+  useEffect(() => {
+    const raw = localStorage.getItem('todos') || []
+    setTodos(JSON.parse(raw))
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
 
   const addTodo = event => {
     if (event.key === 'Enter') {
